7bb99a99002f29d63f4de35b9802090c0a919e00
Paul Forrester <paul@forrester.org>
2016-07-19 21:47:16 -0700

    Made walking algorithm better.  Now automatically calculates distance between two waypoints.  Then directs trainer to walk at 11.5 MPH at 2 steps per second to reach destination.

--- runAutoClicker.py
+++ runAutoClicker.py
@@ -3,17 +3,19 @@ import urllib2
 import json
 import time
 import sys
+from math import cos, sin, atan2, sqrt, pi, radians
 
-perc_ponds = (37.2687140272739, -121.952868728497) # perc ponds
-home = (37.2706395341357, -121.968670287041) # home
-santana_row = (37.321999710354, -121.948474833221) # santana row
-moffet = (37.4188916849005, -122.050083924735) # moffett field
-start = home
-finish = santana_row
-steps= 9000
-step = ((finish[0]-start[0])/steps, (finish[1]-start[1])/steps)
-walk_time = 10.0*60.0
-sleep_time = 1.0/(steps/walk_time)
+
+def calc_distance(w1, w2):
+    earth_radius = 3959
+    lat1, lat2, lon1, lon2 = map(radians, [w1[0], w2[0], w1[1], w2[1]])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    d = earth_radius * c
+    return d
 
 def checkConnected():
     try:
@@ -21,6 +23,7 @@ def checkConnected():
         return json.load(response)
     except urllib2.URLError as e:
         print e.reason
+        raise
 
 
 def clickAction(click_sleep_time=1, n=0):
@@ -37,13 +40,34 @@ def write_gpx(loc):
     del f
 
 
+perc_ponds = (37.2687140272739, -121.952868728497) # perc ponds
+home = (37.2706395341357, -121.968670287041) # home
+santana_row = (37.321999710354, -121.948474833221) # santana row
+moffet = (37.4188916849005, -122.050083924735) # moffett field
+start = home
+finish = moffet
+speed_mph = 11.5
+steps_per_sec = 2.0
+distance = calc_distance(start, finish)
+steps = int(distance/speed_mph * 3600 * steps_per_sec)
+sleep_time = 0.5
+step = ((finish[0]-start[0])/steps, (finish[1]-start[1])/steps)
+
+print "distance: %f" % distance
+print "steps: %d" % steps
+print "step size: (%f, %f)" % step
+
 def walk():
+    counter = 0
     current = list(start)
     for ii in xrange(steps):
         write_gpx(current)
         clickAction(sleep_time, ii)
         current[0] += step[0]
         current[1] += step[1]
+        counter += 1
+        #if not (counter % 10):
+            #checkConnected()
 
 
 def do_auto():
